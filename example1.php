6b986f477de7248bb2384a1f7191cfef36dafea9
railalefan <railalefan@gmail.com>
2015-07-25 12:55:47 +0100

    Updated for version 2015-05-14

--- example1.php
+++ example1.php
@@ -1,11 +1,7 @@
 <?php
   require("OpenLDBWS.php");
-
   $OpenLDBWS = new OpenLDBWS("YOUR_ACCESS_TOKEN");
-
-  $departureBoard = $OpenLDBWS->GetDepartureBoard(10,"PAD");
-
+  $response = $OpenLDBWS->GetDepartureBoard(10,"PAD");
   header("Content-Type: text/plain");
-
-  print_r($departureBoard);
+  print_r($response);
 ?>
